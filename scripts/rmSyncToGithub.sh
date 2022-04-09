ceff52f7198203f07dea15c5c9daf49bda07db06
cori <cori@glitch.com>
2022-04-09 14:09:15 -0500

    another move

--- scripts/rmSyncToGithub.sh
+++ scripts/rmSyncToGithub.sh
@@ -18,4 +18,4 @@ git add .
 git commit -a -m "$(date -u --rfc-3339=s) updates"
 
 #4. git push
-GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no -i /data/id_ed25519 -vvvv" git push
+GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no -i /data/id_ed25519" git push
