aaebcf15568cdc5c5ab463c0550be5ab2dd80e7f
cori schlegel <cori.schlegel@gmail.com>
2022-04-09 15:27:23 +0000

    oops; --backup to the /repo

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ git add .
 git commit -a -m "$(date -u --rfc-3339=s) updates"
 
 #2. run remarkable cli command
-remarkable-cli -a pull -d 192.168.1.105 -u root --password IAMGROOT
+remarkable-cli -a pull -d 192.168.1.105 -u root --password IAMGROOT -b /repo
 
 #3. add all files to git; commit
 git add .
