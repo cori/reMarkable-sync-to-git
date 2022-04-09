c4d3ba251d343fe694e160c0ca00d61befd9b1de
cori schlegel <cori.schlegel@gmail.com>
2022-04-09 19:19:15 +0000

    shell script cleanup
    - proper setup
    - export the ssh config for all git commands
    - commit changes then pull
    
    2022-04-09 20:25:02+00:00 updates
    
    2022-04-09 20:29:47+00:00 updates

--- scripts/rmSyncToGithub.sh
+++ scripts/rmSyncToGithub.sh
@@ -1,21 +1,30 @@
-echo 'comitting and pulling from the reMarkable'
+#!/bin/bash
+set -euo pipefail
+
+echo '*+*+* 	starting the process'
+export GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no -i /data/id_ed25519"
+
 cd /repo
 git status
 
-#0? git pull?
-git remote -v
-git pull
-
 #1. add current files to git, commit
+echo '*+*+* 	comitting loose changes'
 git add .
 git commit -a -m "$(date -u --rfc-3339=s) updates"
 
+#0? git pull?
+echo '*+*+* 	pulling from the remote'
+git pull
+
 #2. run remarkable cli command
+echo '*+*+*	pulling from the reMarkable'
 remarkable-cli -a pull -d 192.168.1.105 -u root --password IAMGROOT -b /repo
 
 #3. add all files to git; commit
+echo '*+*+*	comitting new changes'
 git add .
 git commit -a -m "$(date -u --rfc-3339=s) updates"
 
 #4. git push
-GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no -i /data/id_ed25519" git push
+echo '*+*+*	pushing'
+git push
