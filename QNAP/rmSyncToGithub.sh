f25daa75e4a159d4b8801d3282f8c27287a8cd8e
cori <cori@glitch.com>
2022-04-10 17:17:11 -0500

    move some files around; sh cleanup; copy run script

--- QNAP/rmSyncToGithub.sh
+++ QNAP/rmSyncToGithub.sh
@@ -2,8 +2,6 @@
 set -euo pipefail
 
 notify () {
-#   FAILED_COMMAND="$(caller): ${BASH_COMMAND}" \
-#     echo "Failed command: ${FAILED_COMMAND} -- $?"
     echo "Error on line ${LINENO}: ${BASH_COMMAND}"
 }
 
@@ -15,24 +13,19 @@ export GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no -i /data/id_ed25519"
 cd /repo
 git status
 
-#1. add current files to git, commit
 echo '*+*+* 	comitting loose changes'
 git add .
 git diff-index --quiet HEAD || git commit -a -m "$(date -u --rfc-3339=s) updates"
 
-#0? git pull?
 echo '*+*+* 	pulling from the remote'
 git pull
 
-#2. run remarkable cli command
 echo '*+*+*	pulling from the reMarkable'
 remarkable-cli -a pull -d 192.168.1.105 -u root --password IAMGROOT -b /repo
 
-#3. add all files to git; commit
 echo '*+*+*	comitting new changes'
 git add .
 git diff-index --quiet HEAD || git commit -a -m "$(date -u --rfc-3339=s) updates"
 
-#4. git push
 echo '*+*+*	pushing'
 git push
