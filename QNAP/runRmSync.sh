e2ccd7445a0a509948249fbb5fb499ec218b0307
cori schlegel <46317+cori@users.noreply.github.com>
2022-08-01 06:17:23 -0500

    fix up client ip

--- QNAP/runRmSync.sh
+++ QNAP/runRmSync.sh
@@ -4,5 +4,5 @@ LOCAL_REPO_PATH="/share/homes/cori/repos/reMarkable-contents"
 KEY_PATH="/share/homes/cori/repos/id_ed25519"
 # REMARKABLE_IP="$3"
 REMARKABLE_ROOT_PWD="$1"
-SSH_CLIENT_IP="$SSH_CLIENT | cut -d' ' -f1"
+SSH_CLIENT_IP=$(echo $SSH_CLIENT | cut -d' ' -f1)
 docker run -it --rm --mount type=bind,source=$LOCAL_REPO_PATH,target=/repo --mount type=bind,source=$KEY_PATH,target=/data/id_ed25519 -env "$SSH_CLIENT_IP" -env "$REMARKABLE_ROOT_PWD" remprocessor 
